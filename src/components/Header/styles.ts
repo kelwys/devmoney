38d4047eb84f7d147938771bd072441bc7cecdec
Kelwy <kelwyduarte@gmail.com>
2021-03-30 16:53:20 -0300

    fix header height

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,14 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   background: var(--gray);
-  height: 13rem;
 `;
 
 export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
 
-  padding: 2rem 1rem 0;
+  padding: 2rem 1rem 12rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
