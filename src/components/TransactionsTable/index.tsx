dd1676bab27316eab7654b345bf06f5e3a1f0b41
Kelwy <kelwyduarte@gmail.com>
2021-03-30 17:37:41 -0300

    Add axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions').then(response => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
