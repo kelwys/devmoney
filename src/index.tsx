cdf93c659a8cc43133882ab37de044608681df89
Kelwy <kelwyduarte@gmail.com>
2021-03-30 17:37:13 -0300

    Set fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transação 1',
+          amount: 500,
+          type: 'deposit',
+          category: 'Comida',
+          creatData: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
