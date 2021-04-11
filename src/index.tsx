e0010e1b301df577b989ccc55fe0634c638ae993
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-04-10 21:33:58 -0300

    create transaction models, seeds end post route

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Frelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-04-10 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2021-04-05 09:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transação 1',
-          amount: 500,
-          type: 'deposit',
-          category: 'Comida',
-          creatData: new Date(),
-        },
-      ];
+      return this.schema.all('transaction');
+    });
+
+    this.post('transaction', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   },
 });
