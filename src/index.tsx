eb9a696837f836b590faf36d0638c2211e5dfbb9
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-04-18 20:34:27 -0300

    update post url

--- src/index.tsx
+++ src/index.tsx
@@ -38,7 +38,7 @@ createServer({
       return this.schema.all('transaction');
     });
 
-    this.post('transaction', (schema, request) => {
+    this.post('transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data);
